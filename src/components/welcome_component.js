2dc632fbde414562ddd00ef7b7d62641320eecda
Einav Carmon <nushicarmon@gmail.com>
2017-03-21 14:34:33 +0200

    beating stephen in his own game

--- src/components/welcome_component.js
+++ src/components/welcome_component.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { changeUserName } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 
-const welcomeComp = (props) => {
-    return (
-        <div className="about-me-class" onClick={ () => props.changeUserName()}>
-        <h1 className="headline"> Welcome {props.userName} </h1>
-        </div>
-    );
-};
+class welcomeComp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {counter: 0};
+
+    }
+
+    onHover(event) {
+        console.log('hoverrrrr' + this.state.counter);
+        this.setState({counter : this.state.counter+1});
+    }
+
+    render () {
+        return (
+            <div className="about-me-class" onClick={() => this.props.changeUserName()}>
+                <h1 className="headline"> Welcome {this.props.userName} </h1>
+                <h2 onMouseOver={this.onHover.bind(this)}> Hover over me</h2>
+            </div>
+        );
+    }
+}
 
 function mapStateToProps(store) {
     return { userName: store.userName }
 
 }
+
 function mapDispatchToProps(dispatch) {
     return bindActionCreators( { changeUserName: changeUserName },dispatch)
-    
+
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(welcomeComp);
